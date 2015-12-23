3192748ff2c7b7fca78bd480b06d57f4e0e6d0de
Admin <admin@mac-admin.local>
2015-12-23 21:27:30 +0200

    Initial Commit

---
+++ DrawingsHomework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawingsHomework
+//
+//  Created by Admin on 23.12.15.
+//  Copyright © 2015 Alina Egorova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DrawingsHomework/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DrawingsHomework
+//
+//  Created by Admin on 23.12.15.
+//  Copyright © 2015 Alina Egorova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
